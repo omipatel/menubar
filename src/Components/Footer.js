09f4a4f5dd180316a5f4da7b3ccb854d6f1b52ae
omicoolest <pomkarpatel@gmail.com>
2024-02-19 10:02:31 +0530

    dot com

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,5 @@
 import React from 'react'; 
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBIcon,
-  MDBBtn
-} from 'mdb-react-ui-kit';
-
+ 
 export default function Footer() {
   return (
     
@@ -13,7 +7,7 @@ export default function Footer() {
       <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2024 Copyright:
         <a className='text-dark' href='https://google.com/'>
-          MDBootstrap.com
+          google.com
         </a>
       </div>
  
